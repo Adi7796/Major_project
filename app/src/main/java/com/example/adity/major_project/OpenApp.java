f29c5ebaae60ae4e1533f232e84531300a9abeb4
Adi7796 <adityamaverick8796@gmail.com>
2018-10-28 11:53:16 +0530

    first commit (open app activity)

--- app/src/main/java/com/example/adity/major_project/OpenApp.java
+++ app/src/main/java/com/example/adity/major_project/OpenApp.java
@@ -1,13 +1,33 @@
 package com.example.adity.major_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.app.Activity;
+import android.widget.Button;
 
-public class OpenApp extends AppCompatActivity {
+public class OpenApp extends Activity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_open_app);
+        button=(Button) findViewById(R.id.b1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginActivity();
+            }
+        });
+
+    }
+    public void LoginActivity(){
+        Intent intent= new Intent(this,LoginActivity.class);
+        startActivity(intent);
     }
 }
